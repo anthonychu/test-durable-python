174098b6b5e73103ad1ca478a299a07dfcd8b3bf
Anthony <antchu@microsoft.com>
2020-03-13 17:37:08 +0000

    Some changes

--- HttpTrigger/__init__.py
+++ HttpTrigger/__init__.py
@@ -3,8 +3,11 @@ import logging
 import azure.functions as func
 import azure.durable_functions as df
 
+
 async def main(req: func.HttpRequest, starter: str) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
     client = df.DurableOrchestrationClient(starter)
-    instance_id = await client.start_new("HelloOrchestrator", None, "Seattle")
+    instance_id = await client.start_new(req.route_params["functionName"], None, None)
+
+    logging.info(f"Started orchestration with ID = '{instance_id}'.")
+
     return client.create_check_status_response(req, instance_id)
